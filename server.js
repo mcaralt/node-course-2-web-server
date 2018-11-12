39d9744a78a771574d4797645666b51822c4771c
unknown <a@a.com>
2018-11-12 21:06:57 +0100

    setup strat script and heroku port

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const hbs = require("hbs");
+const port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -24,4 +25,6 @@ app.get("/bad", (req,res) => {
 });
 
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server up on port ${port}`);
+});
